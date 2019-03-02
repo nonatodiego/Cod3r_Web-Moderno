38dc34f25e4e0bfa9518d0fcddd71ea7f49d3ce3
Diego Nonato <nonatodiego@hotmail.com>
2019-03-01 21:42:32 -0300

    add math,strings & templateString

---
+++ EXERCICIOS-JS/strings.js
@@ -0,0 +1,18 @@
+const escola = "cod3r"
+
+console.log(escola.charAt(4))
+console.log(escola.charAt(5))
+console.log(escola.charCodeAt(3)) 
+console.log(escola.indexOf(3))
+
+console.log(escola.substring(1))
+console.log(escola.substring(0,3))
+
+console.log('Eu faço o curso da '.concat(escola).concat("!"))
+console.log('Eu faço o curso da '+ escola + "!")
+console.log(escola.replace(3, 'e'))
+
+console.log('Ana,Maria,Pedro'.split(','))
+
+//string é uma cadeia de caracteres
+
